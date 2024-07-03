48e6ff22090210984af9e83cb424d97556a54322
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-07-03 20:27:53 +0300

    Initial settings

--- app/sources/Main/Screens/Simple/SimpleViewController.swift
+++ app/sources/Main/Screens/Simple/SimpleViewController.swift
@@ -1,17 +1,17 @@
 //
-//  ViewController.swift
+//  SimpleViewController.swift
 //  SimpleName
 //
-//  Created by Stanislav Lemeshaev on 19.06.2024.
+//  Created by Stanislav Lemeshaev on 02.07.2024.
 //  Copyright © 2024 iOS Diving. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class SimpleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .cyan
     }
 }
